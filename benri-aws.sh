f40a2106b2982c3a26cab69b94552ada5fba17b6
Kazumasa Shigeta <shigeta@kanamei.co.jp>
2014-10-30 17:37:42 +0900

    修正

--- benri-aws.sh
+++ benri-aws.sh
@@ -1,11 +1,60 @@
 #!/bin/sh
-
+#できるだけbash依存しないで行こう！
 
 
 benri_aws(){
   #TODproxy t_benri_aws_*
   echo "not implemented."
 }
+_benri_aws_converter_to_snake(){
+  echo "$1"|sed -e 's/\([A-Z]\)/-\1/g' -e 'y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/' -e 's/^-//'
+}
+
+
+#破壊的！作りかけ
+_benri_aws_clear_all_under_vpc(){
+  _vpc_id="$1"
+  for _target in $_BENRI_AWS_ORDER_IN_VPC
+  do
+    :
+  done
+}
+#わかりしだい増やす。作りかけ
+_BENRI_AWS_ORDER_IN_VPC='instance:instances:Inctance:Inctances
+security-group:security-groups:SecurityGroup:SecurityGroups
+subnet:subnets:Subnet:Subnets
+route_table:route_tables:RouteTable:RouteTables
+network-acl:network-acls:NetworkAcl:NetworkAcls
+vpc:vpcs:Vpc:Vpcs
+'
+#作りかけ
+_benri_aws_name_to_plural(){
+  _target_name="$1"
+  _index=0
+  for _names in $_BENRI_AWS_ORDER_IN_VPC
+  do
+    _name=
+    if [ $_name == $_target_name ]
+      then
+      break
+    fi
+    _index=$( expr "$_index" + 1 )
+  done
+  
+  
+}
+_BENRI_AWS_ORDER_IN_VPC_PLURAL='instances
+security-groups
+subnets
+route_tables
+network-acls
+vpcs
+'
+
+
+
+
+
 ## ex. _benri_aws_set_tags_to 'subnet-1234' 'Key=session,Value=12345'
 _benri_aws_set_tags_to(){
   if [ $# -lt 2 ]
@@ -43,8 +92,45 @@ _benri_aws_find_instances_by_tag(){
   aws ec2  "describe-$_target" --query "*[].$_Target[?Tags[?Key==\`$_tagkey\`]][]|[?Tags[?Value==\`$_tagvalue\`]]|[].$_key_name_for_id"  --output text
 }
 
+_benri_aws_find_by_vpcid_from_category(){
+  _vpcid=$1
+  _catregory=$2
+  
+  
+}
+
+#vpcidと関連のあるものすべて
+_benri_aws_find_all_by_vpcid(){
+  _vpcid=$1
+  shift
+  echo "{"
+  for _key in $_BENRI_AWS_EC2_SEARCH_TARGETS 
+   do
+     _query_str='*[]|[?VpcId==`'"$_vpcid"'`]'
+     _ret=
+    _ret=$(aws ec2 "$_key" --query "$_query_str" "$@" )
+    if [ -n "$_ret" ]
+      then
+      
+     echo "$_key : $_ret"
+     echo ","
+   else
+     #階層を下げる
+     _query_str='*[]|[].*|[]|[]|[?VpcId]|[?VpcId==`'"$_vpcid"'`]'
+     _ret=$(aws ec2 "$_key" --query "$_query_str" "$@" )
+     if [ -n "$_ret" ]
+       then
+       
+      echo "$_key : $_ret"
+      echo ","
+    fi
+     
+   fi
+  done
+  echo "}"
+}
+
 _benri_aws_find_route_tables_by_vpcid(){
-#$1 key $2 name
 _vpcid=$1
 
 _target="route-tables"
@@ -54,7 +140,7 @@ _key_name_for_id="RouteTableId"
  aws ec2  "describe-$_target" --query "$_Target[?VpcId==\`$_vpcid\`].$_key_name_for_id" --output text
  }
  
- _benri_aws_find_something_by_tag(){
+ _benri_aws_find_object_by_tag(){
    _tagkey="$1"
    _tagvalue="$2"
    
@@ -71,7 +157,7 @@ _key_name_for_id="RouteTableId"
  _benri_aws_find_ids_by_tag(){
    if [ $# -ne 5 ]
      then
-     echo "_benri_aws_find_ids_by_tag test ttt vpcs Vpcs VpcId"
+     echo "ex. _benri_aws_find_ids_by_tag test ttt vpcs Vpcs VpcId"
      return -1
    fi
    
@@ -87,6 +173,28 @@ _key_name_for_id="RouteTableId"
 
     aws ec2  "describe-$_target" --query "*[]|$(_benri_aws_query_builder_get_object_from_list_by_tag $_tagkey $_tagvalue)|[].$_key_name_for_id"  --output text
  }
+ 
+ #vpc配下にあるもの用
+ _benri_aws_find_ids_by_vpcid_and_tag(){
+   if [ $# -ne 6 ]
+     then
+     echo "ex. _benri_aws_find_ids_by_tag vpc-123456 tagkey tagvalue subnets Subnets SubnetId"
+     return -1
+   fi
+   
+   _tagkey="$1"
+   _tagvalue="$2"
+   
+   #ex. _target="route-tables"
+   _target="$3"
+   #ex. _Target="RouteTables"
+   _Target="$4"
+   #ex. _key_name_for_id="RouteTableId"
+   _key_name_for_id="$5"
+
+    aws ec2  "describe-$_target" --query "*[]|$(_benri_aws_query_builder_get_object_from_list_by_tag $_tagkey $_tagvalue)|[].$_key_name_for_id"  --output text
+ }
+ 
 #依存関係を探すときの助けになるもの。
 _benri_aws_ec2_search_id(){
   if [ $# -eq 0 ]
@@ -110,6 +218,17 @@ _benri_aws_ec2_search_id(){
 
 }
 
+#引数が必要なものは入っていない
+_benri_aws_ec2_describe_all(){
+
+for key in ''$_BENRI_AWS_EC2_SEARCH_TARGETS 
+ do
+   aws ec2 "$key" "$@"
+done
+
+}
+
+##TODOもれがないか
 _BENRI_AWS_EC2_SEARCH_TARGETS='describe-account-attributes
 describe-addresses
 describe-availability-zones
@@ -137,53 +256,75 @@ describe-vpcs
 describe-vpn-connections
 describe-vpn-gateways
 '
-# _BENRI_AWS_EC2_SEARCH_TARGETS='describe-account-attributes
-# describe-addresses
-# describe-availability-zones
-# describe-bundle-tasks
-# describe-conversion-tasks
-# describe-customer-gateways
-# describe-dhcp-options
-# describe-export-tasks
-# describe-image-attribute
-# describe-images
-# describe-instance-attribute
-# describe-instance-status
-# describe-instances
-# describe-internet-gateways
-# describe-key-pairs
-# describe-network-acls
-# describe-network-interface-attribute
-# describe-network-interfaces
-# describe-placement-groups
-# describe-regions
-# describe-reserved-instances
-# describe-reserved-instances-listings
-# describe-reserved-instances-modifications
-# describe-reserved-instances-offerings
-# describe-route-tables
-# describe-security-groups
-# describe-snapshot-attribute
-# describe-snapshots
-# describe-spot-datafeed-subscription
-# describe-spot-instance-requests
-# describe-spot-price-history
-# describe-subnets
-# describe-tags
-# describe-volume-attribute
-# describe-volume-status
-# describe-volumes
-# describe-vpc-attribute
-# describe-vpc-peering-connections
-# describe-vpcs
-# describe-vpn-connections
-# describe-vpn-gateways
-# '
 
+_BENRI_AWS_EC2_DESCRIBES_ALL='describe-account-attributes
+describe-addresses
+describe-availability-zones
+describe-bundle-tasks
+describe-conversion-tasks
+describe-customer-gateways
+describe-dhcp-options
+describe-export-tasks
+describe-image-attribute
+describe-images
+describe-instance-attribute
+describe-instance-status
+describe-instances
+describe-internet-gateways
+describe-key-pairs
+describe-network-acls
+describe-network-interface-attribute
+describe-network-interfaces
+describe-placement-groups
+describe-regions
+describe-reserved-instances
+describe-reserved-instances-listings
+describe-reserved-instances-modifications
+describe-reserved-instances-offerings
+describe-route-tables
+describe-security-groups
+describe-snapshot-attribute
+describe-snapshots
+describe-spot-datafeed-subscription
+describe-spot-instance-requests
+describe-spot-price-history
+describe-subnets
+describe-tags
+describe-volume-attribute
+describe-volume-status
+describe-volumes
+describe-vpc-attribute
+describe-vpc-peering-connections
+describe-vpcs
+describe-vpn-connections
+describe-vpn-gateways
+'
+#１階層下げる
+_benri_aws_query_builder_down_one_level(){
+  ##TODO テストまだ
+  echo "[].*|[]|[]"
+  # echo "[].*[?VpcId]|[]|[]" こっちのほうがいいか？
+}
+_benri_aws_query_builder_get_object_by_keyname(){
+  ##TODO テストまだ
+  _keyname="$1"
+  echo "[?$_keyname]"
+}
 
 _benri_aws_query_builder_get_object_from_list_by_tag(){
   #配列が来ている仮定 たいていの場合このまえに *[]| などをつけるとよいかも
   _keyname=$1
   _value=$2
   echo "[?Tags[?Key==\`$_keyname\`]][]|[?Tags[?Value==\`$_value\`]]"
+}
+_benri_aws_query_builder_get_object_from_list_by_vpcid(){
+  #配列が来ている仮定 たいていの場合このまえに *[]| などをつけるとよいかも
+  _vpcid=$1
+  _query_str='[?VpcId==`'"$_vpcid"'`]'
+  echo "$_query_str"
+}
+#上より1階層深い場所にある場合instanceなど。
+_benri_aws_query_builder_get_object_from_list_by_vpcid_2(){
+  _query_str=''$(_benri_aws_query_builder_down_one_level)'|'$(_benri_aws_query_builder_get_object_from_list_by_vpcid "$@")''
+  echo "$_query_str"
 }
